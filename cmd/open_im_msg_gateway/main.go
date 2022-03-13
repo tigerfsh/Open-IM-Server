18acfb7ac12381e09086323eaed3ef1ff7864728
tigerfsh <mrsmish@yahoo.com>
2022-03-13 23:33:12 +0800

    update at 20220213 23:33

--- cmd/open_im_msg_gateway/main.go
+++ cmd/open_im_msg_gateway/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"Open_IM/internal/msg_gateway/gate"
 	"flag"
-	"sync"
 )
 
 func main() {
 	rpcPort := flag.Int("rpc_port", 10400, "rpc listening port")
 	wsPort := flag.Int("ws_port", 17778, "ws listening port")
 	flag.Parse()
-	var wg sync.WaitGroup
-	wg.Add(1)
+
+	//等价的实现
+	ch := make(chan int)
 	gate.Init(*rpcPort, *wsPort)
 	gate.Run()
-	wg.Wait()
+	ch <- 1
+
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// gate.Init(*rpcPort, *wsPort)
+	// gate.Run()
+	// wg.Wait()
 }
--- internal/msg_gateway/gate/init.go
+++ internal/msg_gateway/gate/init.go
@@ -5,8 +5,9 @@ import (
 	"Open_IM/pkg/common/log"
 	"Open_IM/pkg/statistics"
 	"fmt"
-	"github.com/go-playground/validator/v10"
 	"sync"
+
+	"github.com/go-playground/validator/v10"
 )
 
 var (
@@ -22,9 +23,11 @@ func Init(rpcPort, wsPort int) {
 	//log initialization
 	log.NewPrivateLog(config.Config.ModuleName.LongConnSvrName)
 	rwLock = new(sync.RWMutex)
+	// TODO 读写锁的使用方式，实现细节（次优先级），RWMutex vs Lock+Mutex
 	validate = validator.New()
 	statistics.NewStatistics(&sendMsgCount, config.Config.ModuleName.LongConnSvrName, fmt.Sprintf("%d second recv to msg_gateway sendMsgCount", sendMsgCount), 300)
 	statistics.NewStatistics(&userCount, config.Config.ModuleName.LongConnSvrName, fmt.Sprintf("%d second add user conn", userCount), 300)
+	// TODO 了解statistics（次优先级）
 	ws.onInit(wsPort)
 	rpcSvr.onInit(rpcPort)
 }
